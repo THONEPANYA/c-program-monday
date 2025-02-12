cf3b6251adb3417a0451790d68c981a16299ad89
THONEPANYA <sihapanya.inth@gmail.com>
2025-02-12 13:30:20 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    printf("********* Stduent Information *********\n\t");
+    printf("> Name: INTHONE\n\t");
+    printf("> Surname: SIHAPANYA\n\t");
+    printf("> Age: 21 Year olds\n\t");
+    printf("> Year: 1 Afternoon\n\t");
+    printf("> \"Soutsaka\"\n\t");
+    printf("> Percentage = 100%%\n");
+    printf("****************************************\n");
+    return 0;
+}
