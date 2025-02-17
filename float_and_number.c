ffbe87f5a4d9baeb0d63acbd183013715a57c844
THONEPANYA <sihapanya.inth@gmail.com>
2025-02-17 14:55:59 +0700

    Create float_and_number.c

---
+++ float_and_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+
+    float number = 30.5;
+    int number1 = 5;
+
+    printf("The Value = %.1f\n", number);
+    printf("The Value = %.1f > The Value %d\n", number, number1);
+
+    return 0;
+}
